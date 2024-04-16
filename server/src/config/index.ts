4e9a45c29005c5925fe7bcddebed7d5340ffb120
MatanAv <matanav757@gmail.com>
2024-04-16 12:15:37 +0300

    fixed configuration

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -3,8 +3,8 @@ const config = {
     port: process.env.PORT || 8080
   },
   db: {
-    BASE_URI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`,
-    URI_PARAMS: `?retryWrites=true&w=majority&appName=${process.env.DB_NAME}`
+    BASE_URI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}` || '',
+    URI_PARAMS: `?retryWrites=true&w=majority&appName=${process.env.DB_NAME}` || ''
   }
 };
 
