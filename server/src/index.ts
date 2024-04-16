fca92e1d06ce050a19727fc45f34a8064c86559e
MatanAv <matanav757@gmail.com>
2024-04-16 12:36:27 +0300

    added cors + test route

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,10 +6,17 @@ import 'dotenv/config';
 import express from 'express';
 import config from '@/config';
 import connectDB from '@/config/db';
+import cors from 'cors';
 
 const app = express();
 
 connectDB();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(config.app.port, () => {
   console.log(`Server is running on port ${config.app.port}...`);
