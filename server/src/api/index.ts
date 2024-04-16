ff14e6a6a60b99b00d4eae84d46de315618343b7
MatanAv <matanav757@gmail.com>
2024-04-16 14:08:59 +0300

    Refactor server configuration and add vercel-build script

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,17 +1,3 @@
-import express from 'express';
-import config from '@/config';
-import cors from 'cors';
-
-const app = express();
-
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(config.app.port, () => {
-  console.log(`Server is running on port ${config.app.port}...`);
-});
+import app from '@/config/app';
 
 export default app;
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,12 @@ if (process.env.NODE_ENV !== 'development') {
 }
 
 import 'dotenv/config';
-import './api';
+import config from '@/config';
 import connectDB from '@/config/db';
+import app from '@/config/app';
 
 connectDB();
+
+app.listen(config.app.port, () => {
+  console.log(`Server is running on port ${config.app.port}...`);
+});
