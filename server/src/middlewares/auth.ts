7284e400e6c2187004bf9bddb0e4ec9e8ef76d3e
MatanAv <matanav757@gmail.com>
2024-05-02 18:29:52 +0300

    Add logging statements to authMiddleware

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -2,6 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  console.log('authMiddleware Test:');
+  console.log(req);
+  console.log(req.session);
+  console.log(req.sessionID);
+  console.log(req.session.worker_id);
+
   if (!req.session.worker_id) {
     return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Unauthorized' });
   } else {
