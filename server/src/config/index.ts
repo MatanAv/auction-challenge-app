43014f1abcd077308fe4c5ac16fea22ec8634e84
MatanAv <matanav757@gmail.com>
2025-05-24 13:09:05 +0300

    chore: add yarn.lock file with updated dependencies

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,21 +1,24 @@
 const config = {
-  app: {
-    port: process.env.PORT || 8080,
-    session: {
-      secret: process.env.SESSION_SECRET,
-      cookie: {
-        maxAge: Number(process.env.SESSION_MAX_AGE) || 1000 * 60 * 60 * 24, // 1 day,
-        domain: process.env.SESSION_DOMAIN || 'localhost'
-      }
+    app: {
+        port: process.env.PORT || 8080,
+        session: {
+            secret: process.env.SESSION_SECRET,
+            cookie: {
+                maxAge: Number(process.env.SESSION_MAX_AGE) || 1000 * 60 * 60 * 24, // 1 day,
+                domain: process.env.SESSION_DOMAIN || 'localhost'
+            }
+        }
+    },
+    db: {
+        BASE_URI:
+            process.env.NODE_ENV === 'production'
+                ? `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`
+                : `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}`,
+        URI_PARAMS: `?retryWrites=true&w=majority&appName=${process.env.DB_NAME}` || ''
+    },
+    game: {
+        approvalKey: process.env.APPROVAL_KEY || '1234'
     }
-  },
-  db: {
-    BASE_URI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}` || '',
-    URI_PARAMS: `?retryWrites=true&w=majority&appName=${process.env.DB_NAME}` || ''
-  },
-  game: {
-    approvalKey: process.env.APPROVAL_KEY || '1234'
-  }
 };
 
 export default config;
